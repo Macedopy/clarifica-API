db1d2f7fd7c79bfb4a1ed4053968209fb0062dcd
Bruno Macedo <brunomacedolemos15@gmail.com>
2025-11-10 20:01:13 -0300

    Teams

--- micro-management-contruction/src/main/java/construction/foundation/Foundation.java
+++ micro-management-contruction/src/main/java/construction/foundation/Foundation.java
@@ -6,6 +6,10 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
+
+import java.sql.Date;
+
+import construction.components.used_material.Material;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 
 @Entity
@@ -15,41 +19,23 @@ public class Foundation extends PanacheEntityBase {
     private String id;
 
     @Column(name = "first_name", nullable = false)
-    private String firstName;
+    private String name;
 
     @Column(name = "last_name", nullable = false)
-    private String lastName;
+    private Date date;
 
     @Email(message = "Invalid email format")
     @Column(nullable = false, unique = true)
     private String email;
 
     @Column(nullable = false)
-    private String phone;
+    private int percentageCompleted;
 
-    @Pattern(regexp = "\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}", message = "CPF must be in the format XXX.XXX.XXX-XX")
-    @Column(nullable = false, unique = true)
-    private String cpf;
+    @Column(nullable = false)
+    private Material usedMaterials;
 
     public String getId() { return id; }
     public void setId(String id) { this.id = id; }
 
-    public String getFirstName() { return firstName; }
-    public void setFirstName(String firstName) { this.firstName = firstName; }
-
-    public String getLastName() { return lastName; }
-    public void setLastName(String lastName) { this.lastName = lastName; }
-
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-
-    public String getPhone() { return phone; }
-    public void setPhone(String phone) { this.phone = phone; }
-
-    public String getCpf() { return cpf; }
-    public void setCpf(String cpf) { this.cpf = cpf; }
-
-    public String fullName() {
-        return firstName + " " + lastName;
-    }
+    
 }
